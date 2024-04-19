7cebd04aa812d4d944a6ad93483be6b2b2a5c892
Najla STITOU <najla@powertofly.com>
2024-04-19 17:34:31 +0300

    GI-design-pattern-1: rename description for AlfArmy

--- abstract-factory/src/main/java/com/iluwatar/abstractfactory/ElfArmy.java
+++ abstract-factory/src/main/java/com/iluwatar/abstractfactory/ElfArmy.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public class ElfArmy implements Army, ArmyMembers {
 
-  static final String DESCRIPTION = "This is the elven army!";
+  static final String DESCRIPTION_ELF = "This is the elven army!";
 
   @Override
   public String getDescription() {
